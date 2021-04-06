f1235f31c9a6631ae52ad971bc4f68eed61c2b10
Kanghyeon Kim <bebekh1216@gmail.com>
2021-04-06 15:35:41 +0900

    index 페이지 구성중

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,70 @@
 import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
+import FilterComponent from '../components/FilterComponent';
+import TableComponent from '../components/TableComponent';
 import styles from '../styles/Home.module.css'
+import * as api from './api/api'
+
+export const DataDispatch = React.createContext({});
 
 export default function Home() {
+  const [patientData, setPatientData] = useState([]) 
+  const [tableParams, setTableParams] = useState({
+    currentPage: 1,
+    currentRow: 10,
+  })
+  const [maxPage, calcMaxPage] = useState(1)
+  const [filterCategory, setFilterCategory] = useState({
+    gender: [],
+    ethnicity: [],
+    race: [],
+  });
+
+  
+  // 컴포넌트 하나에서 모든 상태를 관리
+  // 함수를 나눠서 Provider로 관리하는 방법도 생각
+
+  // 렌더링 후 처음 한번 실행
+  // 필터 카테고리를 가져오는 과정은 여러번 할 필요가 없음
+  useEffect(() => {
+    // console.log("First Rendering!")
+    api.getFilterCategory().then(res => {
+      // 카테고리에 None 용도로 데이터 추가
+      Object.values(res).map(element => {
+        Object.values(element).map(arr => {
+          arr.unshift("---");
+        })
+      })
+      setFilterCategory({
+        ethnicity: res.ethnicity.ethnicityList,
+        gender: res.gender.genderList,
+        race: res.race.raceList,
+      })
+      console.log(res)
+    });
+  }, [])
+  
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>React Data Viewer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* Home 에 전역변수를? */}
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            
+          {/* 그래프 */}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          {/* 필터 */}
+          <FilterComponent category={filterCategory}></FilterComponent>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          {/* 테이블 */}
+          <TableComponent value={patientData}></TableComponent>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
       </footer>
     </div>
   )
